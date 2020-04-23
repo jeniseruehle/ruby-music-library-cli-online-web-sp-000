e94d56ba3929ef4404c4ade1e66d4d328cf237e6
Jenise Ruehle <jela07@gmail.com>
2020-04-23 14:11:11 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -27,4 +27,13 @@ class Artist
     artist.save
     artist
   end 
+  
+  def add_song(song)
+    song.artist = self unless song.artist
+    songs << song unless songs.include?(song) 
+  end
+
+  def genres
+    songs.collect{|s| s.genre}.uniq
+  end
 end 

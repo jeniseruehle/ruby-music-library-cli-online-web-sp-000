bb9896e48e4a084e6141666b0981fef8d8a436e7
Jenise Ruehle <jela07@gmail.com>
2020-04-23 13:39:05 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -46,9 +46,9 @@ class MusicLibraryController
   end
   
   def list_artists
-    Artist.all.sort {|a, b| a.name <=> b.name}.each_with_index(1) do |x, i|
-      puts "#{i}. #{x.name}"
-    end 
+    Artist.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |a, i|
+        puts "#{i}. #{a.name}"
+    end
   end
   
 end 
